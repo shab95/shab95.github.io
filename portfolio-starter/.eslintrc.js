5b5d54fb26f2393b37f1d9963d71c6f5af71f948
Shabarish Nair <snair95@umd.edu>
2023-10-12 19:25:13 -0400

    Started about page

--- portfolio-starter/.eslintrc.js
+++ portfolio-starter/.eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
-  "extends": "airbnb-base",
-  "env": {
-    "browser": true,
-    "node": true
+  extends: "airbnb-base",
+  env: {
+    browser: true,
+    node: true,
   },
   rules: {
     "no-console": "off",
-  }
+    "linebreak-style": 0,
+  },
 };
