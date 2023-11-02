d2976c4b6947dfb182607853ad490558860023b7
Shabarish Nair <snair95@umd.edu>
2023-11-02 15:04:28 -0400

    contact page and linting

--- portfolio-starter/.eslintrc.js
+++ portfolio-starter/.eslintrc.js
@@ -1,13 +1,19 @@
+const standard = require("eslint-config-standard");
 module.exports = {
-  extends: "airbnb-base",
+  extends: ["standard"],
   env: {
     browser: true,
     node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: "module",
   },
   rules: {
     "no-console": "off",
     "linebreak-style": 0,
     "no-param-reassign": [2, { props: false }],
     "no-unused-vars": "off",
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
   },
 };
--- portfolio-starter/src/javascripts/index.js
+++ portfolio-starter/src/javascripts/index.js
@@ -1,15 +1,28 @@
-const TabLinks = document.getElementsByClassName('tab-links');
-const TabContents = document.getElementsByClassName('tab-contents');
+const TabLinks = document.getElementsByClassName('tab-links')
+const TabContents = document.getElementsByClassName('tab-contents')
 
-function openAboutTabs(tab) { // eslint-disable-line no-unused-vars
+function openAboutTabs (tab) { // eslint-disable-line no-unused-vars
   for (let i = 0; i < TabLinks.length; i += 1) {
-    const element = TabLinks[i];
-    element.className = element.id === tab ? 'tab-links active-link' : 'tab-links';
+    const element = TabLinks[i]
+    element.className = element.id === tab ? 'tab-links active-link' : 'tab-links'
   }
-  event.currentTarget.classList.add('active-link'); // eslint-disable-line no-restricted-globals
+  event.currentTarget.classList.add('active-link') // eslint-disable-line no-restricted-globals
 
   for (let i = 0; i < TabContents.length; i += 1) {
-    const element = TabContents[i];
-    element.className = element.id === tab ? 'tab-contents active-tab' : 'tab-contents';
+    const element = TabContents[i]
+    element.className = element.id === tab ? 'tab-contents active-tab' : 'tab-contents'
   }
 }
+
+async function sleep (ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms)) // eslint-disable-line no-promise-executor-return
+}
+
+const form = document.forms['submit-form']
+const statusMsg = document.getElementById('status')
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+  statusMsg.innerHTML = 'Thanks for your message'
+  await sleep(2500)
+  statusMsg.innerHTML = ''
+})
